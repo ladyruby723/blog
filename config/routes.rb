6175674ce4d31c286f8fae251af3e060ddb306c9
Kate Richardson <krichardson723@gmail.com>
2017-03-01 17:05:27 -0700

    add Contributor resource with devise and add sign_up/sign_in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :contributors
+  as :contributor do 
+    get "signin" => 'devise/sessions#new'
+    delete "signout" => 'devise/sessions#destroy'
+    get "signup" => 'devise/registrations#new'
+  end
   root 'pages#home'
   get 'about' => 'pages#about' # this hash rocket creates a key-value connection between the GET action for a page and the page location (path)
   get 'contact' => 'pages#contact'
