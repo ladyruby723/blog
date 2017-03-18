78a6391a37e17aba5103f2b436fc3ee9d066bb81
Kate Richardson <krichardson723@gmail.com>
2017-03-17 18:00:40 -0600

    add username to post views

--- app/models/contributor.rb
+++ app/models/contributor.rb
@@ -3,4 +3,9 @@ class Contributor < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :rememberable, :trackable, :validatable
+
+  has_many :posts
+
+  validates :username, presence: true, uniqueness: true, length: { minimum: 6 }
+  validates :name, presence: true
 end
